709882cdd2fa19f4b6265bc0c99cae4813501101
Jordan Cedeño <gatocedejordan2005@gamail.com>
2021-09-17 20:09:12 -0500

    Taller interfaz list

---
+++ src/main/java1/elercicios/coleccio/list/Main.java
@@ -0,0 +1,28 @@
+package elercicios.coleccio.list;
+
+import java.util.List;
+import java.util.ArrayList;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Integer numero1=1;
+		Integer numero2=3;
+		Integer numero3=5;
+		Integer numero4=7;
+		Integer numero5=13;
+		Integer numero6=113;
+
+		
+		List<Integer> listaNueva=new ArrayList<Integer>();
+		listaNueva.add(numero1);
+		listaNueva.add(numero2);
+		listaNueva.add(numero3);
+		listaNueva.add(numero4);
+		listaNueva.add(numero5);
+		listaNueva.add(numero6);
+
+	}
+
+}
